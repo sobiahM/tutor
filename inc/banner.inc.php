0fddef093f0cff62b531f5ef01ec1d4c82b78ba2
sofia <Sobiahmurugan@gmail.com>
2022-11-11 15:24:51 +0530

    sofi cute uh

--- inc/banner.inc.php
+++ inc/banner.inc.php
@@ -1,2 +1,2 @@
-<h1><a href="index.php">Sofi<span> Tutor</span></a></h1>
-<h3><a href="#">Contact: panadi@eruma.maadu</a></h3>
+<h1><a href="index.php">HIRE<span> TUTOR</span></a></h1>
+<h3><a href="#">Contact: </a></h3>
